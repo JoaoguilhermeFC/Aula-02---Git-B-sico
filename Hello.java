748298fb8cb4a84af0e1fcc96ae974c9a66213c0
JoaoguilhermeFC <jgfcancelier@gmail.com>
2026-03-05 16:16:56 -0300

    Vriando a class Hello

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        ola();
+        
+    }
+    public static void ola(){
+         System.out.println("Hello World");
+    }
+
+}
