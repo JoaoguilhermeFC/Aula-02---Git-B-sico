8df64b834b8c3f9808cb721e57566d3b4524a9b3
JoaoguilhermeFC <jgfcancelier@gmail.com>
2026-03-05 16:21:01 -0300

    Criando a class Alemon

---
+++ Alemon.java
@@ -0,0 +1,5 @@
+public class Alemon {
+    public static void AlemonUm(){
+        System.out.println("Hallo Welt!");
+    }
+}
--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         ola();
-        
+        Alemon.AlemonUm();
     }
     public static void ola(){
          System.out.println("Hello World");
