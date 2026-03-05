3e17a2611de12049232e98b8c5d388004ff442dd
JoaoguilhermeFC <jgfcancelier@gmail.com>
2026-03-05 16:26:17 -0300

    Criando a class Italiaano

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         ola();
-        
+        Italiaano.ItaliaanoUm();
     }
     public static void ola(){
          System.out.println("Hello World");
---
+++ Italiaano.java
@@ -0,0 +1,5 @@
+public class Italiaano {
+    public static void ItaliaanoUm(){
+        System.out.println("Ciao Mondo!");
+    }
+}
